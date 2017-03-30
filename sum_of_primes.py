365c96d7a9bcad211ed1dde5bfe3774399cf0988
BasicCode <tommy_tom2000@hotmail.com>
2017-03-30 22:19:01 +1100

    Sum of Primes

--- sum_of_primes.py
+++ sum_of_primes.py
@@ -13,17 +13,48 @@ def primesUpToN(n):
 			for j in range(2, math.ceil((n / i))):
 				p = p + i
 				isPrime[p] = False
+	
 	return isPrime
 
+#Find the longest sum of primes possible
+def sumOfPrimes(isPrime):
+	#Turn the array of indexes in to an array of numbers
+	primes = []
+	for i in range(2, len(isPrime)):
+		if(isPrime[i]):
+			primes.append(i)
+	
+	numPrimes = len(primes)
+	
+	#test each sequence of primes up to half the total number of primes
+	sums = []
+	highest_prime = 0
+	for i in range(0, math.ceil(numPrimes / 2)):
+		testSum = 0
+		sumSequence = []
+		#sum a sequence of primes
+		j = i
+		while testSum < primes[-1] - highest_prime:
+			testSum = testSum + primes[j]
+			sumSequence.append(primes[j])
+			#if the sum is TRUE in the array then it is a prime
+			if(isPrime[testSum]):
+				print(sumSequence)
+				print(testSum)
+				sums.append(sumSequence)
+				highest_prime = sumSequence[-1]
+			j = j + 1
+		
+	return sums
+
 #Print out the primes
+print("Finding primes...")
 start = time.time()
-isPrime = primesUpToN(1000000)
+isPrime = primesUpToN(1000)
 stop = time.time()
 
-counter = 0
-for i in range(2, len(isPrime)):
-	if(isPrime[i]):
-		#print(i)
-		counter = counter + 1
+print("Finding sum of primes...")
+sums = sumOfPrimes(isPrime)
 
-print("Found " + str(counter) + " in " + str(stop - start) + " seconds")
+print("Found " + str(len(primes)) + " in " + str(stop - start) + " seconds")
+print(sums)
